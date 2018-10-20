27455e63545a7bf2267f3b2d548f2b95a06c5fc2
Eric Rohner <ericrohner22@gmail.com>
2018-10-20 02:30:32 -0600

    merge

--- src/GameInterface.js
+++ src/GameInterface.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import merge from 'deepmerge'
 import GameCanvas from './components/GameCanvas'
 import GameControls from './components/GameControls'
 
@@ -8,31 +8,52 @@ class GameInterface extends Component {
     super()
     this._getConfig = this._getConfig.bind(this)
     this.state = {
-      player1Width: 15,
-      player1Height: 40,
-      player1R: 255,
-      player1G: 255,
-      player1B: 255,
-      player1VelocityY: 1,
+      player1: {
+        width: 15,
+        height: 80,
+        color: {
+          hex: 'ffffff',
+          rgb: {
+            b: 255,
+            g: 255,
+            r: 255
+          }
+        },
+        velocityY: 1,
+      },
 
-      player2Width: 15,
-      player2Height: 40,
-      player2R: 255,
-      player2G: 255,
-      player2B: 255,
-      player2VelocityY: 1,
 
-      ballWidth: 15,
-      ballHeight: 15,
-      ballR: 255,
-      ballG: 255,
-      ballB: 255,
-      ballVelocityY: 1,
-      ballVelocityX: 1,
+      player2: {
+        width: 15,
+        height: 80,
+        color: {
+          hex: 'ffffff',
+          rgb: {
+            b: 255,
+            g: 255,
+            r: 255
+          }
+        },
+        velocityY: 1,
+      },
+
+      ball: {
+        width: 15,
+        height: 80,
+        color: {
+          hex: 'ffffff',
+          rgb: {
+            b: 255,
+            g: 255,
+            r: 255
+          }
+        },
+        velocityY: 1,
+        velocityX: 1
+      },
     }
   }
 
-  //
 
   async _getConfig() {
     const resp = await fetch(
@@ -40,23 +61,22 @@ class GameInterface extends Component {
     ).then(data => {
       return data.json()
     })
-    if (resp.gameData.paddle1.width !== undefined) {
-      this.setState({player1Width: resp.gameData.paddle1.width})
-      console.log(this.state.player1Width)
+    //the reply is an object; I'm interested in three child objects.
+    //i trust 'https://wwwforms.suralink.com/pong.php?accessToken=pingPONG' to not screw with me.
+    //therefore: it is safe to update when the children are not arrays.
+    if (!Array.isArray(resp.gameData.paddle1)) {
+      //this is a shallow merge. It merges objects but replaces child objects like player1.color => paddle1.color. This is fine.
+      //shallow merge player1 and paddle1. Set the result to state.
+      this.setState({ player1: { ...this.state.player1, ...resp.gameData.paddle1 } })
     }
-    if (resp.gameData.paddle1.height !== undefined) {
-      this.setState({player1Height: resp.gameData.paddle1.height})
-      console.log(this.state.player1Height)
+    if (!Array.isArray(resp.gameData.paddle2)) {
+      this.setState({ player2: { ...this.state.player2, ...resp.gameData.paddle2 } })
     }
-    if (resp.gameData.paddle2.width !== undefined) {
-      this.setState({player2Width: resp.gameData.paddle2.width})
-      console.log(this.state.player2Width)
-    }
-    if (resp.gameData.paddle2.height !== undefined) {
-      this.setState({player2Height: resp.gameData.paddle2.height})
-      console.log(this.state.player2Height)
+    if (!Array.isArray(resp.gameData.ball)) {
+      this.setState({ ball: { ...this.state.ball, ...resp.gameData.ball } })
     }
     console.log(resp.gameData)
+    //last recursively call self after the delay.
     setTimeout(this._getConfig, resp.gameData.newDelay)
   }
 
@@ -81,6 +101,7 @@ class GameInterface extends Component {
             alignItems: 'center'
           }}
         >
+          /*pass the game config fetch'd from the server to the GameCanvas props*/
           <GameCanvas {...this.state} />
           <GameControls />
         </section>
